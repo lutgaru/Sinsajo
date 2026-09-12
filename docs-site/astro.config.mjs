610e8651476896bd6dfffb56a3acb7e14c6986c4
Adrian Neftali Sanchez <lutgaru@gmail.com>
2026-09-11 20:11:27 -0700

    improve hero section

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 			pagefind: false,
 			components: {
 				Header: './src/components/Header.astro',
+				Hero: './src/components/Hero.astro',
 			},
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/lutgaru/Sinsajo' },
